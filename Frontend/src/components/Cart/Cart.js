8d4564af7de46cf532e34efd0173a0449010172e
zacktaylor12359 <46452540+zacktaylor12359@users.noreply.github.com>
2022-01-11 17:21:34 -0500

    ADD and REMOVE actions complete in context

--- Frontend/src/components/Cart/Cart.js
+++ Frontend/src/components/Cart/Cart.js
@@ -1,13 +1,41 @@
+import { useContext } from 'react';
+
 import styles from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
+
 const Cart = (props) => {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
+
+	const cartItemRemoveHandler = (id) => {
+		cartCtx.removeItem(id);
+	};
+
+	const cartItemAddHandler = (item) => {
+		cartCtx.addItem({
+			id: item.id,
+			name: item.name,
+			amount: 1,
+			price: item.price,
+		});
+	};
+
 	const cartItems = (
 		<ul className={styles['cart-items']}>
-			{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(
-				(item) => (
-					<li key={item.id}>{item.name}</li>
-				)
-			)}
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
 		</ul>
 	);
 	return (
@@ -15,7 +43,7 @@ const Cart = (props) => {
 			{cartItems}
 			<div className={styles.total}>
 				<span>Total Amount</span>
-				<span>24.34</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={styles.actions}>
 				<button
@@ -24,7 +52,7 @@ const Cart = (props) => {
 				>
 					Close
 				</button>
-				<button className={styles.button}>Order</button>
+				{hasItems && <button className={styles.button}>Order</button>}
 			</div>
 		</Modal>
 	);
--- Frontend/src/store/CartProvider.js
+++ Frontend/src/store/CartProvider.js
@@ -9,13 +9,66 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === 'ADD') {
-		const updatedItems = state.items.concat(action.item);
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount;
+
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+		const existingCartItem = state.items[existingCartItemIndex];
+
+		let updatedItems;
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedItems = state.items.concat(action.item);
+		}
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
 		};
+	}
+	if (action.type === 'REMOVE') {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+		const existingCartItem = state.items[existingCartItemIndex];
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount - 1,
+			};
+
+			if (updatedItem.amount < 0) {
+				console.error(
+					updatedItem.name +
+						'returned an amount less than 0. clearing cart'
+				);
+				return defaultCartState;
+			}
+			const updatedItems = [...state.items];
+			if (updatedItem.amount === 0) {
+				updatedItems.splice(existingCartItemIndex, 1);
+			} else {
+				updatedItems[existingCartItemIndex] = updatedItem;
+			}
+
+			const updatedTotalAmount =
+				state.totalAmount - existingCartItem.price;
+
+			return {
+				items: updatedItems,
+				totalAmount: updatedTotalAmount,
+			};
+		}
+		return state;
 	}
 	return defaultCartState;
 };
